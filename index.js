6239a7b97097f4bab798c249ea6c43f064469d8b
bibakroy <roybibak1@gmail.com>
2023-05-05 15:13:44 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/mern-shopping-app")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error: ", err.message);
+  });
 
 // Start server
 app.listen(5000, () => {
