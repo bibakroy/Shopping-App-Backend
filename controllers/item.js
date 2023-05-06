415dc2ecef0cd676f100f5251f23f472b6970b98
bibakroy <roybibak1@gmail.com>
2023-05-06 22:19:57 +0600

    create get-items api

--- controllers/item.js
+++ controllers/item.js
@@ -1,5 +1,17 @@
 import Item from "../models/item.js";
 
+export const getItems = async (req, res) => {
+  try {
+    const items = await Item.find({ created_by: req.user.userId }).sort({
+      created_at: -1,
+    });
+
+    return res.status(201).json(items);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
 export const addItem = async (req, res) => {
   const { name } = req.body;
 
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ import express from "express";
 const router = express.Router();
 
 import { register, login } from "../controllers/user.js";
-import { addItem } from "../controllers/item.js";
+import { getItems, addItem } from "../controllers/item.js";
 import { verifyToken } from "../controllers/verifyToken.js";
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
 
+router.get("/get-items", verifyToken, getItems);
 router.post("/add-item", verifyToken, addItem);
 
 export default router;
